e873e5febe78821035b8c49ffed5a57e5f47a9d2
gowtham <128037284+gowthampp@users.noreply.github.com>
2023-03-29 12:36:56 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import './App.css';
+import D4DC from './D4DC';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <center>
+          <D4DC/>
+        </center>
+      </div>
+    )
+  }
+}
+export default App
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
